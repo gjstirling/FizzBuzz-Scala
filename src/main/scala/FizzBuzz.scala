4ab7d7e7739a8c99425d4508796a9a7211b4474d
Graeme Stirling <graemejstirling@gmail.com>
2022-07-26 09:20:13 +0100

    Green Second test

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,5 +1,11 @@
 object FizzBuzz {
   def generate(number: Int): String = {
-    "1"
+    var a = 0;
+    var fizzBuzzArray = Array[String]()
+    for( a <- 1 to number){
+      fizzBuzzArray = fizzBuzzArray :+ a.toString
+    }
+    var arrayString = fizzBuzzArray.mkString(", ")
+    arrayString
   }
 }
